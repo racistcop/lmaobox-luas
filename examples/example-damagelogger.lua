29d29a22a6b51fe770b3858443991e8d635baf2b
RC <63776072+racistcop@users.noreply.github.com>
2022-03-15 12:22:32 +0800

    update: example-damagelogger

--- examples/example-damagelogger.lua
+++ examples/example-damagelogger.lua
@@ -1,18 +1,18 @@
 local function damageLogger(event)
 
-    if (event:GetName() == 'player_hurt' ) then
+    if (event:GetName() == 'player_hurt' ) then -- https://wiki.alliedmods.net/Team_Fortress_2_Events#player_hurt
         
-        local localPlayer = entities.GetLocalPlayer()
         local victim = entities.GetByUserID(event:GetInt("userid"))
         local health = event:GetInt("health")
         local attacker = entities.GetByUserID(event:GetInt("attacker"))
         local damage = event:GetInt("damageamount")
         
-        if (attacker == nil or localPlayer:GetIndex() ~= attacker:GetIndex()) then
+        if (attacker == nil or entities.GetLocalPlayer():GetIndex() ~= attacker:GetIndex()) then -- prevents printing damage taken
             return
         end
 
         print("You hit " ..  victim:GetName() .. " or ID " .. victim:GetIndex() .. " for " .. damage .. "HP they now have " .. health .. "HP left")
+
     end
 
 end
